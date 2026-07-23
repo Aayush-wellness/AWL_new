ab9379021e44ca89494d61b7ff63b28cc2a0933f
Rohit Saundalkar <soundrohit632@gmail.com>
2026-07-23 15:26:41 +0530

    added clarity script to html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Manrope } from "next/font/google";
 import { Header, Footer, ClientInitializer } from "@/components/layout";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -33,6 +34,19 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} ${manrope.variable}`} suppressHydrationWarning>
       <body suppressHydrationWarning>
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "ntqa8a1t21");
+            `,
+          }}
+        />
         <ClientInitializer />
         <Header />
         {children}
